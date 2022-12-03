e37a82d3b9c2bf0abebb80d79c9518f229b99ffb
n-s-iroagbba <nnamdisolomon1@gmail.com>
2022-12-03 13:43:11 +0100

    update features

--- takum/client/src/admin/AdminLogin.js
+++ takum/client/src/admin/AdminLogin.js
@@ -5,7 +5,6 @@ import Form from 'react-bootstrap/Form'
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import { useNavigate } from "react-router-dom";
-import axios from 'axios'
 
 
 const AdminLogin = () => {
