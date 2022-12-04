c51d7312db446b0f078e01ae13ba74128adff9af
n-s-iroagbba <nnamdisolomon1@gmail.com>
2022-12-04 12:20:48 +0100

    [feat]:fix bug

--- takum/client/src/ViewContestant/female_contestant/F_ViewFemaleDetails.js
+++ takum/client/src/ViewContestant/female_contestant/F_ViewFemaleDetails.js
@@ -34,7 +34,7 @@ const FemaleDetails = ({props,setPage}) => {
         
             
                 <Card style={{ width: '80vw',height:'100vh' }} variant="top" className='card shadow-sm m-3 p-2 rounded'>
-                        <Card.Img style={{ height:'60vw',width:'60vh',objectFit: 'scale-down', marginTop: '0%' }} className='img' src={`https://takum.fly.dev/${image}`}/>
+                        <Card.Img style={{ height:'60vh',width:'80vw',objectFit: 'scale-down', marginTop: '0%' }} className='img' src={`https://takum.fly.dev/${image}`}/>
                         <Card.Body>
                             <Card.Title className='text-dark'>Contestant:{id}</Card.Title>
                             <Card.Text className="text-success">{firstName.toUpperCase()} {lastName.toUpperCase()}</Card.Text>
