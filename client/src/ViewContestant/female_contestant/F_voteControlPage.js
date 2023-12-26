b63cd6839bd98690bd16774c00de0b15658b799b
IROAGBA NNAMDI SOLOMON <104698116+n-s-iroagba@users.noreply.github.com>
2023-12-27 00:46:09 +0100

    Update F_voteControlPage.js

--- client/src/ViewContestant/female_contestant/F_voteControlPage.js
+++ client/src/ViewContestant/female_contestant/F_voteControlPage.js
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
+import Reactfrom "react";
 import ViewFemale from './F_ViewFemale'
 import FVotePayForm from "./F_VotePayForm";
 
 //used to control view between male contestants and votepay form
 const FVoteControlPage = () =>{
-    const [votePage,setVotePage] = useState(false);
-    const setPage = () => setVotePage(true);
+    //const [votePage,setVotePage] = useState(false);
+//const setPage = () => setVotePage(true);
 
     const VotePageRender = () => {
         switch (votePage){
-            case false : return <ViewFemale setPage={setPage}/>
+            case false : return <ViewFemale/>
             case true : return <FVotePayForm/>
             default : return <ViewFemale/>
         }
